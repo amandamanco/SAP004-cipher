2d0dba13fd0d3caed69702c654adc22a172bc93f
Amanda Manço <amand113@hotmail.com>
2020-04-09 18:00:10 -0300

    Projeto Pronto - Teste Rodado

--- src/decode.js
+++ src/decode.js
@@ -1,10 +1,15 @@
 import cipher from './cipher.js';
 
 
-document.getElementById("user-text-decoded").addEventListener("keyup", function () {
-    var offsetDecode = document.getElementById("offsetDecode").value; //Peguei o id do HTML e guardei em uma variável
+document.getElementById("user-text-decoded").addEventListener("input", function () {
+    var offsetDecode = Number(document.getElementById("offsetDecode").value); //Peguei o id do HTML e guardei em uma variável
     var userTextDecoded = document.getElementById("user-text-decoded").value.toUpperCase(); //Peguei o id do texto digitado, deixei em maiusculo e guardei em uma variável
     var resultText = document.getElementById("decode-text");//"Separei" o espaço no HTML para o resultado
-    var result = cipher.decode(offsetDecode, userTextDecoded);//Coloquei as variaveis gaurdadas como parametro para a função do cipher
+    var result = cipher.decode(offsetDecode, userTextDecoded); //Coloquei as variaveis gaurdadas como parametro para a função do cipher
     resultText.innerHTML = result; //Printei o resultado no HTML
+    
 });
+
+// if(offsetDecode=="") {
+    //alert("Insira o número do offset!")
+//}
--- src/encode.js
+++ src/encode.js
@@ -1,13 +1,17 @@
 import cipher from './cipher.js';
 
 
-document.getElementById("user-text-encoded").addEventListener("keyup", function () {
-  var offsetEncode = document.getElementById("offsetEncode").value;
+document.getElementById("user-text-encoded").addEventListener("input", function () {
+  var offsetEncode = Number(document.getElementById("offsetEncode").value);
   var userTextEncoded = document.getElementById("user-text-encoded").value.toUpperCase();
   var resultText = document.getElementById("encoded-text");
   var result = cipher.encode(offsetEncode, userTextEncoded);
   resultText.innerHTML = result;
-
+  
 });
 
 
+
+// if(offsetEncode=="") {
+    //alert("Insira o número do offset!")
+//}
