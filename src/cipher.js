d0247614357ba185fe97383b11ac12a3f89307a6
Amanda Manço <amand113@hotmail.com>
2020-04-11 22:22:18 -0300

    Commit com o código finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -2,9 +2,9 @@ const cipher = {
 
 
   encode: (offsetEncode, strEncode) => { //Peguei o offset e a string digitados pelo usuário
-    if (offsetEncode=="" || typeof offsetEncode!=="number" || typeof strEncode!=="string") { //Fiz a condição para que desse Error quando a string fosse vazia ou não string e o offset fosse vazio ou não numero.
+    if (offsetEncode == "" || typeof offsetEncode !== "number" || typeof strEncode !== "string") { //Fiz a condição para que desse Error quando a string fosse vazia ou não string e o offset fosse vazio ou não numero.
       throw new TypeError('Insira o número do offset ou confira que seu texto não tenha caracteres especiais') //Mensagem de erro
-    } // *****Esse if roda o teste 100% mas não executa a função no navegador.
+    }
     var solved = ""; //Deixei a variavel do resultado declarada
     for (var i = 0; i < strEncode.length; i++) { //Inicnpmiei o for com o contador, parametro e o modificador
       var asciiNum = strEncode[i].charCodeAt(); //Modifiquei o "numero da string" para ser compativel com a do ASCII
@@ -16,10 +16,7 @@ const cipher = {
 
 
   decode: (offsetDecode, strDecode) => {
-    if (offsetDecode=="" || typeof offsetDecode!=="number" || typeof strDecode!=="string") { 
-      throw new TypeError('Insira o número do offset ou confira que seu texto não tenha caracteres especiais') 
-    }
-        var solved = "";
+    var solved = "";
     for (var i = 0; i < strDecode.length; i++) {
       var asciiNum = strDecode[i].charCodeAt();
       var caesarNum = (asciiNum + 65 - offsetDecode) % 26 + 65;
--- src/encode.js
+++ src/encode.js
@@ -1,17 +1,18 @@
 import cipher from './cipher.js';
 
 
-document.getElementById("user-text-encoded").addEventListener("input", function () {
+document.getElementById("user-text-encoded").addEventListener("keyup", function () {
   var offsetEncode = Number(document.getElementById("offsetEncode").value);
   var userTextEncoded = document.getElementById("user-text-encoded").value.toUpperCase();
   var resultText = document.getElementById("encoded-text");
+  if (offsetEncode == "") {
+    alert("Insira o número do offset!")
+  }
   var result = cipher.encode(offsetEncode, userTextEncoded);
   resultText.innerHTML = result;
-  
+
 });
 
 
 
-// if(offsetEncode=="") {
-    //alert("Insira o número do offset!")
-//}
+
