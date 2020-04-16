566a2e2bc6021db5f808c80550afcc632f0497b7
Amanda Manço <amand113@hotmail.com>
2020-04-16 15:49:19 -0300

    Primeira Refatoração

--- src/encode.js
+++ src/encode.js
@@ -2,13 +2,13 @@ import cipher from './cipher.js';
 
 
 document.getElementById("user-text-encoded").addEventListener("keyup", function () {
-  var offsetEncode = Number(document.getElementById("offsetEncode").value);
-  var userTextEncoded = document.getElementById("user-text-encoded").value.toUpperCase();
-  var resultText = document.getElementById("encoded-text");
-  if (offsetEncode == "") {
+  const offsetEncode = Number(document.getElementById("offsetEncode").value);
+  const userTextEncoded = document.getElementById("user-text-encoded").value.toUpperCase();
+  const resultText = document.getElementById("encoded-text");
+  if (offsetEncode === "") {
     alert("Insira o número do offset!")
   }
-  var result = cipher.encode(offsetEncode, userTextEncoded);
+  let result = cipher.encode(offsetEncode, userTextEncoded);
   resultText.innerHTML = result;
 
 });
